99377e3fc464bbb03bce68fb44f3a51bff4e571f
williamhsu17 <williamhsu17@gmail.com>
2016-11-18 16:19:43 +0800

    devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,27 +4,11 @@ class PostsController < ApplicationController
   Num_posts_per_page = 10
 
   def index
-    if params[:status] == "new"
-      @post = Post.new
-
-    elsif params[:status] == "update"
-      set_post
-    else
-    end
-
-    if params[:page]
-      @total_page = Post.all.page(1).per(Num_posts_per_page).total_pages
-      params[:page] = @total_page if params[:page].to_i > @total_page
-    end
-
     @posts = Post.page(params[:page]).per(Num_posts_per_page)
     @posts = @posts.includes(:category)
-    @number_count = (@posts.current_page - 1) * Num_posts_per_page
   end
   def new
-    redirect_to posts_path( :status => :new,
-                            :set_post_path => posts_path,
-                            :set_method => :post)
+
   end
   def edit
     redirect_to posts_path( :id => params[:id],
@@ -37,22 +21,10 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
-    # @post.user = current_user
-    if @post.save
-      flash[:notice] = "新增主題成功"
-      redirect_to posts_path
-    else
-      render new_post_path
-    end
+
   end
   def update
-    if @post.update(post_params)
-      redirect_to post_path(@post)
-      flash[:notice] = "更新成功"
-    else
-      render "posts/edit"
-    end
+
   end
   def destroy
     @post.destroy
