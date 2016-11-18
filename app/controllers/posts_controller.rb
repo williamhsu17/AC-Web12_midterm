815a22a39fa1aebbf416b0b4f22d72dfc474105b
williamhsu17 <williamhsu17@gmail.com>
2016-11-18 17:17:19 +0800

    login system

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, :except => [:index]
   before_action :set_post, :only => [:edit, :show, :update, :destroy]
 
   Num_posts_per_page = 10
